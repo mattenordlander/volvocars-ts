5ec38877d4cc4c1a90f1c1b9edadf5627defda1c
Mathias Nordlander <mattsson.nord@gmail.com>
2024-08-14 12:54:13 +0200

    refactor: integrate AgendaCards and render agenda data in EventSchedule component

--- app/components/VolvoPage/EventSchedule.tsx
+++ app/components/VolvoPage/EventSchedule.tsx
@@ -1,5 +1,18 @@
-export default function EventSchedule(){
-    return(
-<h1>Event Scheddule</h1>
-    )
+import AgedanCards from "../AgendaCards";
+import agendaData from "./data/agenda.json";
+
+export default function EventSchedule() {
+  return (
+    <div className=" p-5">
+      <h1>Event Scheddule</h1>
+      {agendaData.map(({ time, title, speaker, description }) => (
+        <AgedanCards
+          time={time}
+          speaker={speaker}
+          title={title}
+          description={description}
+        />
+      ))}
+    </div>
+  );
 }
