35aa245d09b6c0bcb24656be22f9646a5c3b537b
Mathias Nordlander <mattsson.nord@gmail.com>
2024-08-14 13:30:23 +0200

    fix: add unique key prop to agenda items in EventSchedule component

--- app/components/AgendaCards.tsx
+++ app/components/AgendaCards.tsx
@@ -4,13 +4,12 @@ type AgendaProp = {
     title: string;
     speaker:string;
     description:string;
-    card:number
 }
 
 
-export default function agedanCards({time, title, speaker, description, card}: AgendaProp){
+export default function agedanCards({time, title, speaker, description}: AgendaProp){
     return(
-        <div key={card} className="flex-col border-b-2 border-slate-200 my-8 pb-7">
+        <div className="flex-col border-b-2 border-slate-200 my-8 pb-7">
             <h1 className="text-5xl font-bold mb-8">{time}</h1>
             <div className="mb-5">
             <h2 className="text-xl font-bold ">{title}</h2>
--- app/components/VolvoPage/EventSchedule.tsx
+++ app/components/VolvoPage/EventSchedule.tsx
@@ -7,7 +7,7 @@ export default function EventSchedule() {
       <h1>Event Scheddule</h1>
       {agendaData.map(({ time, title, speaker, description, card }) => (
         <AgedanCards
-      card={card}
+      key={card}
           time={time}
           speaker={speaker}
           title={title}
