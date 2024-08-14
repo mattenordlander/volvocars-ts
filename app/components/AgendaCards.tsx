d4b23431cfd59c70b4fcd39dd5daab5a31676239
Mathias Nordlander <mattsson.nord@gmail.com>
2024-08-14 12:52:49 +0200

    feat: create AgendaCards component

---
+++ app/components/AgendaCards.tsx
@@ -0,0 +1,22 @@
+
+type AgendaProp = {
+    time: string;
+    title: string;
+    speaker:string;
+    description:string;
+}
+
+
+export default function agedanCards({time, title, speaker, description}: AgendaProp){
+    return(
+        <div className="flex-col border-b-2 border-slate-200 my-8 pb-7">
+            <h1 className="text-5xl font-bold mb-8">{time}</h1>
+            <div className="mb-5">
+            <h2 className="text-xl font-bold ">{title}</h2>
+            <h2 className="text-slate-500">{description}</h2>
+            </div>
+            
+            <p className="text-slate-500">{speaker}</p>
+        </div>
+    )
+}
