fafa52f60d2c792784dfc4e9a1e53ac179e4724c
Mathias Nordlander <mattsson.nord@gmail.com>
2024-08-21 12:16:13 +0200

    ref: Minor syntax clean up

--- app/components/VolvoPage/MainContent.tsx
+++ app/components/VolvoPage/MainContent.tsx
@@ -9,12 +9,12 @@ import { db } from "../../utils/firebase";
 
 type mainContentProp = {
     title:string;
-    description:string
+    description:string;
 }
 
 export default function MainContent({title, description}:mainContentProp){
 
-const [state, setState] = useState<string>("before");
+const [state, setState] = useState<string>('before');
 
 useEffect(() => {
     const docRef = doc(db, "StreamStatus", "state");
@@ -25,6 +25,7 @@ useEffect(() => {
       if (data) {
         setState(data.state);
       }
+
     });
   
    
@@ -33,11 +34,7 @@ useEffect(() => {
   
     return(
 <> 
-{state === "before" ? (
-    <BeforeMain />
-) : (
-<LiveMain />
-)}     
+{state === "before" ? <BeforeMain /> : <LiveMain />}   
         <div className="w-100 flex align-center justify-center flex-col px-6 gap-y-10 my-10">
             <h1 className="text-center text-6xl font-bold">{title}</h1>
             <p className="text-center">{description}</p>
