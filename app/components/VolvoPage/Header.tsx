048611d182638d49499a3e8b065254743c52b5ec
Mathias Nordlander <mattsson.nord@gmail.com>
2024-08-22 10:49:35 +0200

    fix: change text from 'CH' to 'SWE'

--- app/components/VolvoPage/Header.tsx
+++ app/components/VolvoPage/Header.tsx
@@ -9,7 +9,7 @@ export default function Header({toggleLanguage}:headerProp){
     return(
         <div className=' flex items-center px-4 py-5 justify-between'>
             <img src="/images/black_volvologo.svg" alt="" />
-            <button onClick={toggleLanguage}>CN | EN</button>
+            <button onClick={toggleLanguage}>SWE | EN</button>
         </div>
     )
 }
